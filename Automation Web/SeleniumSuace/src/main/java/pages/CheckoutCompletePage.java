f194362d3de6fc7cc8a91e892c90da2789058dfa
NicolasRiv24 <152893064+NicolasRiv24@users.noreply.github.com>
2024-09-23 09:54:46 -0400

    Check Out Complete Page Implementation

--- Automation Web/SeleniumSuace/src/main/java/pages/CheckoutCompletePage.java
+++ Automation Web/SeleniumSuace/src/main/java/pages/CheckoutCompletePage.java
@@ -1,4 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 public class CheckoutCompletePage {
+
+    WebDriver driver;
+
+    // Locators
+    public By confirmationMessage = By.className("complete-header");
+    public By backHomeButton = By.id("back-to-products");
+    // Constructor
+    public CheckoutCompletePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Methods
+    public String getConfirmationMessage() {
+        return driver.findElement(confirmationMessage).getText();
+    }
+    public void clickBackToProducts(){
+        driver.findElement(backHomeButton).click();
+
+    }
 }
+
