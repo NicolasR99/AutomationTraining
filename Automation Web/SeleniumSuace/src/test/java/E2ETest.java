40770ba4b49fc3e6b1e09bfade5e1bd6fe8426c3
NicolasRiv24 <152893064+NicolasRiv24@users.noreply.github.com>
2024-09-23 12:01:02 -0400

    Tests: "Web1 to Web7" Implementation

--- Automation Web/SeleniumSuace/src/test/java/E2ETest.java
+++ Automation Web/SeleniumSuace/src/test/java/E2ETest.java
@@ -52,9 +52,55 @@ public class E2ETest {
         checkoutFirstPage = new CheckoutFirstPage(driver);
         checkoutSecondPage = new CheckOutSecondPage(driver);
         checkoutCompletePage = new CheckoutCompletePage(driver);
-        waitUtils = new WaitUtils(driver, 2);
+        waitUtils = new WaitUtils(driver,2);
         sideBar = new SideBar(driver);
     }
+
+
+    @Test(priority = 1, description = "Login to the application (step 1)")
+    @Description("Test Description: Login to the SauceDemo application with valid credentials")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Login")
+    public void testLogin() {
+        Assert.assertTrue(driver.findElement(loginPage.usernameField).isDisplayed(), "Username field present");
+        Assert.assertTrue(driver.findElement(loginPage.passwordField).isDisplayed(), "Password field present");
+        Assert.assertTrue(driver.findElement(loginPage.loginButton).isEnabled(), "Login button enabled");
+        loginPage.login(TestData.username, TestData.password);
+    }
+
+    @Test(priority = 2, description = "Add products to cart")
+    @Description("Test Description: Add the first and last items to the cart and verify")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Add to Cart")
+    public void testAddProductsToCart() {
+        // This code is for adding the first and the last item of the list
+        inventoryPage.addFirstAndLastItemsToCart();
+
+        // Save the names and prices of the first and last item
+        firstItemName = inventoryPage.getFirstItemName();
+        lastItemName = inventoryPage.getLastItemName();
+        firstItemPrice = inventoryPage.getFirstItemPrice();
+        lastItemPrice = inventoryPage.getLastItemPrice();
+        inventoryPage.goToCart();
+
+        // This code is for adding 2 specific items
+/*
+        firstItemName = TestData.firstItemName;
+        lastItemName = TestData.lastItemName;
+        firstItemPrice = TestData.firstItemPrice;
+        lastItemPrice = TestData.lastItemPrice;
+        inventoryPage.addBackpackToCart();
+        inventoryPage.addBikeLightToCart();
+        inventoryPage.goToCart();*/
+
+        waitUtils.waitForUrlContains("/cart.html");
+        Assert.assertTrue(driver.getCurrentUrl().equals("https://www.saucedemo.com/cart.html"), "Redirected to inventory page");
+        Assert.assertTrue(driver.findElement(cartPage.checkoutButton).isDisplayed(), "Checkout button visible");
+
+        List<WebElement> cartItems = driver.findElements(cartPage.cartItemNames);
+        Assert.assertEquals(cartItems.size(), 2, "Number of products in cart is not correct");
+    }
+
     @Test(priority = 3, description = "Review cart")
     @Description("Test Description: Review the cart items and proceed to checkout")
     @Severity(SeverityLevel.NORMAL)
@@ -75,6 +121,7 @@ public class E2ETest {
 
         cartPage.clickCheckoutButton();
     }
+
     @Test(priority = 4, description = "Enter checkout information")
     @Description("Test Description: Enter the user information for checkout")
     @Severity(SeverityLevel.CRITICAL)
@@ -90,6 +137,7 @@ public class E2ETest {
         Assert.assertTrue(driver.findElement(checkoutFirstPage.continueButton).isEnabled(), "Finish button not enabled");
         checkoutFirstPage.clickContinueButton();
     }
+
     @Test(priority = 5, description = "Review order summary")
     @Description("Test Description: Review the order summary and confirm the prices")
     @Severity(SeverityLevel.CRITICAL)
@@ -124,6 +172,7 @@ public class E2ETest {
 
         checkoutSecondPage.clickFinishButton();
     }
+
     @Test(priority = 6, description = "Confirm order")
     @Description("Test Description: Confirm the order and verify the confirmation message")
     @Severity(SeverityLevel.CRITICAL)
@@ -134,5 +183,24 @@ public class E2ETest {
 
         Assert.assertTrue(driver.findElement(checkoutCompletePage.backHomeButton).isEnabled(), "Back Home button not enabled");
         checkoutCompletePage.clickBackToProducts();
+    }
+
+    @Test(priority = 7, description = "Logout from the application")
+    @Description("Test Description: Logout from the SauceDemo application")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Logout")
+    public void testLogout() {
+        Assert.assertTrue(driver.findElement(sideBar.sideBarButton).isEnabled(),"SideBar button not enabled");
+        sideBar.clickSideBard();
+
+        Assert.assertTrue(driver.findElement(sideBar.logOutButton).isEnabled(),"Logout button not enabled");
+        sideBar.clicklogOutButton();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
